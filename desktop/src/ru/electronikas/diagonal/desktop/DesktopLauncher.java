eeda8b6f1e05d17ceb2a85ca04f91366bf2a89ff
nikas <nikita.avdonin@gmail.com>
2016-06-19 14:24:19 +0800

    unut project

---
+++ desktop/src/ru/electronikas/diagonal/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.electronikas.diagonal.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.electronikas.diagonal.Di2048Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Di2048Game(), config);
+	}
+}
---
+++ html/src/ru/electronikas/diagonal/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package ru.electronikas.diagonal.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import ru.electronikas.diagonal.Di2048Game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Di2048Game();
+        }
+}

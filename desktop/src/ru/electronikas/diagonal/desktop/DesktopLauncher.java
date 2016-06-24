29a2c38448a1a2a1a312ef90f830583ff3bfd4ce
nikas <nikita.avdonin@gmail.com>
2016-06-24 19:50:35 +0800

    release 1.0

--- desktop/src/ru/electronikas/diagonal/desktop/DesktopLauncher.java
+++ desktop/src/ru/electronikas/diagonal/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ import java.util.Locale;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		Locale.setDefault(new Locale("ru", "RU"));
-//		Locale.setDefault(new Locale("en", "US"));
+//		Locale.setDefault(new Locale("ru", "RU"));
+		Locale.setDefault(new Locale("en", "US"));
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 700;
 		config.height = 1000;

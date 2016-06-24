6a37e1df27a4ecd439918437259348d94da6a0e4
nikas <nikita.avdonin@gmail.com>
2016-06-24 13:30:07 +0800

    locality

--- desktop/src/ru/electronikas/diagonal/desktop/DesktopLauncher.java
+++ desktop/src/ru/electronikas/diagonal/desktop/DesktopLauncher.java
@@ -4,8 +4,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.electronikas.diagonal.Di2048Game;
 
+import java.util.Locale;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Locale.setDefault(new Locale("ru", "RU"));
+//		Locale.setDefault(new Locale("en", "US"));
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 700;
 		config.height = 1000;
